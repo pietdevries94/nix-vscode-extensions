d5a7363a60a2345a20f51ba41ebb3a0ebed4bec5
pietdevries94 <piet@devries.tech>
2021-01-06 15:53:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nix VSCode extensions
+
+I use this repo to auto update my vscode extensions. You can use this as an overlay and access the packages under `extra-vscode-extensions`.
+
+The update script is based on https://github.com/NixOS/nixpkgs/blob/master/pkgs/misc/vscode-extensions/update_installed_exts.sh
+
+Check extensions.txt for the available extensions. If you miss something just send me a PR.
---
+++ default.nix
@@ -0,0 +1,81 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  inherit (pkgs.vscode-utils) buildVscodeMarketplaceExtension;
+in
+{
+  extra-vscode-extensions = {
+    Bauke.horizon-vscode = buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        name = "horizon-vscode";
+        publisher = "Bauke";
+        version = "3.0.3";
+        sha256 = "0njdlmvrb8pdk81bv92dppvn3jgqhfc7zx6f349f11yxwgs9hwsr";
+      };
+    };
+    arrterian.nix-env-selector = buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        name = "nix-env-selector";
+        publisher = "arrterian";
+        version = "0.1.2";
+        sha256 = "1n5ilw1k29km9b0yzfd32m8gvwa2xhh6156d4dys6l8sbfpp2cv9";
+      };
+    };
+    bbenoist.Nix = buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        name = "Nix";
+        publisher = "bbenoist";
+        version = "1.0.1";
+        sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+      };
+    };
+    bungcip.better-toml = buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        name = "better-toml";
+        publisher = "bungcip";
+        version = "0.3.2";
+        sha256 = "08lhzhrn6p0xwi0hcyp6lj9bvpfj87vr99klzsiy8ji7621dzql3";
+      };
+    };
+    eamodio.gitlens = buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        name = "gitlens";
+        publisher = "eamodio";
+        version = "11.1.3";
+        sha256 = "1x9bkf9mb56l84n36g3jmp3hyfbyi8vkm2d4wbabavgq6gg618l6";
+      };
+    };
+    golang.go = buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        name = "go";
+        publisher = "golang";
+        version = "0.20.1";
+        sha256 = "0hhxgkv7p2hbjqj9q9xdlkcnva6b2lcszcpxj7fy4wfqz1w9lcaj";
+      };
+    };
+    matklad.rust-analyzer = buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        name = "rust-analyzer";
+        publisher = "matklad";
+        version = "0.2.441";
+        sha256 = "1qv52c2ans594zxxl9cv343hdkc292k851p99khdcjr1qcfgcib9";
+      };
+    };
+    naumovs.color-highlight = buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        name = "color-highlight";
+        publisher = "naumovs";
+        version = "2.3.0";
+        sha256 = "1syzf43ws343z911fnhrlbzbx70gdn930q67yqkf6g0mj8lf2za2";
+      };
+    };
+    octref.vetur = buildVscodeMarketplaceExtension {
+      mktplcRef = {
+        name = "vetur";
+        publisher = "octref";
+        version = "0.31.3";
+        sha256 = "02fhmmi08335ky4v5ayf4ra0gzrkrpjghrjpd77bxjikjh98wkdz";
+      };
+    };
+  };
+}
