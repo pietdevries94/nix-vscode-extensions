6ddf1431bdb85886dda6f2b10a9b288f7e3e7c20
pietdevries94 <pietdevries94@users.noreply.github.com>
2022-01-02 00:53:06 +0000

    Apply automatic updates

--- default.nix
+++ default.nix
@@ -145,8 +145,8 @@ in
       mktplcRef = {
         name = "rust-analyzer";
         publisher = "matklad";
-        version = "0.3.886";
-        sha256 = "19y1aqlx17g5a176g3kxmf0j77n424dhkq3y5m1s5d6cqphz8xrc";
+        version = "0.3.887";
+        sha256 = "0sazb2jf7pdhk383273g2fcmk5p63ilijl16jx4q8mdgr3mkvlfl";
       };
     };
     naumovs.color-highlight = buildVscodeMarketplaceExtension {
