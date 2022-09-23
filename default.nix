62cd5c20a79b561ea8a6b8a55c286d676ef5b9e2
Piet de Vries <piet@compenda.nl>
2022-09-23 23:46:47 +0200

    Add adwaita-theme

--- default.nix
+++ default.nix
@@ -25,8 +25,8 @@ in
       mktplcRef = {
         name = "vscode-tailwindcss";
         publisher = "bradlc";
-        version = "0.8.3";
-        sha256 = "09miqfb3vmj382ii3rvl28ncjv3waagk6zxisjhrcslragyr0cgk";
+        version = "0.8.7";
+        sha256 = "1pyd5qwrsy9rnl600jc0x82mviq830fdb916fwz150vk7nyv0fs5";
       };
     };
     bungcip.better-toml = buildVscodeMarketplaceExtension {
@@ -41,24 +41,24 @@ in
       mktplcRef = {
         name = "vscode-github-actions";
         publisher = "cschleiden";
-        version = "0.24.1";
-        sha256 = "1q7jvcc0v9717bnzlhmxr5jppf4yrvgyav3imr5jdqpk6451lxc5";
+        version = "0.24.2";
+        sha256 = "1j3am5x863crqrlhhyyin4ph9pqsgn7vkf1yck65qaa6zg8vzg34";
       };
     };
     dbaeumer.vscode-eslint = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "vscode-eslint";
         publisher = "dbaeumer";
-        version = "2.2.3";
-        sha256 = "0sl9d85wbac3h79a5y5mcv0rhcz8azcamyiiyix0b7klcr80v56d";
+        version = "2.2.6";
+        sha256 = "0m16wi8slyj09r1y5qin9xsw4pyhfk3mj6rs5ghydfnppb45w9np";
       };
     };
     eamodio.gitlens = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "gitlens";
         publisher = "eamodio";
-        version = "12.0.6";
-        sha256 = "1d7gzxsyxrhvvx2md6gbcwiawd8f3jarxfbv2qhj7xl1phd7zja3";
+        version = "2022.8.3105";
+        sha256 = "11axaxi06j0hb9xv35azqy4fys15pllg67gk3sqsc3s66q4qzk6w";
       };
     };
     editorconfig.editorconfig = buildVscodeMarketplaceExtension {
@@ -89,32 +89,32 @@ in
       mktplcRef = {
         name = "vscode-pull-request-github";
         publisher = "github";
-        version = "0.43.2022051220";
-        sha256 = "1mvxyajnlg8jvahszm8z0swdr5k2npdxlidcch8bh83d4gc81a19";
+        version = "0.51.2022092209";
+        sha256 = "1g19qlnb8ffh932qk6sb5j1gcm6vl1i9j0c59k58g3ldsl3wj5i6";
       };
     };
     GitLab.gitlab-workflow = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "gitlab-workflow";
         publisher = "GitLab";
-        version = "3.43.1";
-        sha256 = "0n7qsqbaw8w9spdix37688wxa35002mdg7zi3wwkg4wdnwkhqz5q";
+        version = "3.52.0";
+        sha256 = "0wyc9bd4i5m7gdngckfz74341d11m4y3qqb1s80lb550x2xjbx0l";
       };
     };
     golang.go = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "go";
         publisher = "golang";
-        version = "0.33.0";
-        sha256 = "1wd9zg4jnn7y75pjqhrxm4g89i15gff69hsxy7y5i6njid0x3x0w";
+        version = "0.35.2";
+        sha256 = "1nkf96wsnlaimganx0kn4lhdajpnps5rsm8dhyh0j8vdlw3wl0v1";
       };
     };
     graphql.vscode-graphql = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "vscode-graphql";
         publisher = "graphql";
-        version = "0.4.4";
-        sha256 = "1cj8c8pccn53ivk2hx59vpw4xw3y54vf5bflzsljnm5dl48biz6g";
+        version = "0.7.5";
+        sha256 = "1gnm4968bvib0lwndsmrhb1094q8ay9y77kyj57wdh0mj7s6i6bf";
       };
     };
     gruntfuggly.todo-tree = buildVscodeMarketplaceExtension {
@@ -129,8 +129,8 @@ in
       mktplcRef = {
         name = "nix-ide";
         publisher = "jnoortheen";
-        version = "0.1.20";
-        sha256 = "16mmivdssjky11gmih7zp99d41m09r0ii43n17d4i6xwivagi9a3";
+        version = "0.1.23";
+        sha256 = "0i5zqfz2ic57z9b4v6mrs68dzhvgpnr5984srjapi0nzm30k127b";
       };
     };
     lakshits11.horizon-legacy = buildVscodeMarketplaceExtension {
@@ -141,36 +141,36 @@ in
         sha256 = "13w3sli69p72z93h48kh1vxldk2bcgmz1zbwwh9myb6ifd0f0c6g";
       };
     };
-    matklad.rust-analyzer = buildVscodeMarketplaceExtension {
+    piousdeer.adwaita-theme = buildVscodeMarketplaceExtension {
       mktplcRef = {
-        name = "rust-analyzer";
-        publisher = "matklad";
-        version = "0.3.1051";
-        sha256 = "10vfgn6yx6gx2jbzfmzq8aym9303pn1kzydn96rcyl9ab5b3xaih";
+        name = "adwaita-theme";
+        publisher = "piousdeer";
+        version = "1.0.8";
+        sha256 = "1zz8api1162akh3nzb2nvnjxzz4q87frxawybq43a64h0a5z2b2z";
       };
     };
     naumovs.color-highlight = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "color-highlight";
         publisher = "naumovs";
-        version = "2.5.0";
-        sha256 = "0ri1rylg0r9r1kdc67815gjlq5fwnb26xpyziva6a40brrbh70vm";
+        version = "2.6.0";
+        sha256 = "1ssh5d4kn3b57gfw5w99pp3xybdk2xif8z6l7m3y2qf204wd1hsd";
       };
     };
     octref.vetur = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "vetur";
         publisher = "octref";
-        version = "0.35.0";
-        sha256 = "1l1w83yix8ya7si2g3w64mczh0m992c0cp2q0262qp3y0gspnm2j";
+        version = "0.36.0";
+        sha256 = "14ym7i858j5q4sf0wsr78c6sv8dmgw4i4ah4p22hgx9n89i27k0h";
       };
     };
     svelte.svelte-vscode = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "svelte-vscode";
         publisher = "svelte";
-        version = "105.16.0";
-        sha256 = "18b2wdmj7h9507pzyf05a5k2ll2hiz22xsq2mpw0w4nm14j6v42n";
+        version = "106.1.0";
+        sha256 = "1bxjyqhk424q7rypdwwqr2h2fyxhpyws238y9iry657sxvcrq0w3";
       };
     };
     voorjaar.windicss-intellisense = buildVscodeMarketplaceExtension {
@@ -185,24 +185,24 @@ in
       mktplcRef = {
         name = "vscode-icons";
         publisher = "vscode-icons-team";
-        version = "11.11.0";
-        sha256 = "0qd9y0rb1j70iha8gqkxv2xvds6n4db8p0h8arlqcsfayljkn5v6";
+        version = "11.17.0";
+        sha256 = "1jd9q29pi6m6fng7653hfj7iy8db2lpcc1ql16lhhq1w0yf3z4ar";
       };
     };
     wicked-labs.wvsc-serendipity = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "wvsc-serendipity";
         publisher = "wicked-labs";
-        version = "0.103.0";
-        sha256 = "1dish2j30hsi2kp91vrc3cxfw8pq8rwk0rp84biza9pnk5iqxr5j";
+        version = "0.104.0";
+        sha256 = "11gf124jy8v85p8km0japn19lb2xybjirp8rcpj5xgs0hvx6b726";
       };
     };
     znck.vue-language-features = buildVscodeMarketplaceExtension {
       mktplcRef = {
         name = "vue-language-features";
         publisher = "znck";
-        version = "0.11.5";
-        sha256 = "12x0nkxx2mxhfhf2lqmli13nblprl85p9fq1b0l4gnbc711frnrs";
+        version = "0.11.7";
+        sha256 = "0v7i751jfpyv59c5ydar8c9s2pm8876w0ic1ri0fv2v5kc0qgsdg";
       };
     };
   };
